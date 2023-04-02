38c36ac0ff67547cf520fb509ad679ed7d8247f6
Iago <iago.pontes@outlook.com>
2023-04-02 11:31:40 -0300

    Inclusão variavel do ambiente "MY_PHONE".

--- twilio_api.py
+++ twilio_api.py
@@ -1,3 +1,4 @@
+import os
 from twilio.rest import Client
 
 account_sid = "AC5e91742dd982da7003b3f1d730b54550"
@@ -5,8 +6,7 @@ auth_token = "e0d3b2a8480c6396eaeee67495b1b498"
 client = Client(account_sid, auth_token)
 
 twilio_phone = "+14344258431"
-my_phone = "+5524999551421"
-
+my_phone = os.getenv("MY_PHONE")
 
 def send_sms():
     message = client.messages \
